d5ed8eca088740ce87eed8b74aa7279242608bd7
Mamta Bhatt <610mamtabhatt@gmail.com>
2024-06-06 16:14:45 +0545

    appBar build

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF6B8E23);
+  static const Color secondary = Color(0xFFF4A460);
+  static const Color accentColor = Color(0xFFFFFACD);
+}
