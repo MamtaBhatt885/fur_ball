1ded86787cefa383df46fa40c9c05688266bbf88
Mamta Bhatt <610mamtabhatt@gmail.com>
2024-06-09 21:47:35 +0545

    bottom nav bar

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -4,4 +4,5 @@ class AppColors {
   static const Color primary = Color(0xFF6B8E23);
   static const Color secondary = Color(0xFFF4A460);
   static const Color accentColor = Color(0xFFFFFACD);
+  static const Color whiteColor = Color(0xFFFFFFFF);
 }
